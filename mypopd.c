f7be1ca3c9b18d2a4a67f3ef525e801cc5ae8752
eros <erosrodv@gmail.com>
2024-10-17 20:12:01 -0700

    changed noop logic

--- mypopd.c
+++ mypopd.c
@@ -27,6 +27,8 @@ typedef struct serverstate {
     State state;
     struct utsname my_uname;
     // TODO: Add additional fields as necessary
+    char current_user[MAX_USERNAME_SIZE];  // Add this field to store the username
+
 } serverstate;
 
 static void handle_client(void *new_fd);
@@ -75,10 +77,34 @@ int do_quit(serverstate *ss) {
     return -1;
 }
 
-int do_user(serverstate *ss) {
-    dlog("Executing user\n");
-    // TODO: Implement this function
-    return 0;
+// int do_user(serverstate *ss) {
+//     dlog("Executing user\n");
+//     // TODO: Implement this function
+//     return 0;
+// }
+int do_user(serverstate *ss, const char *username) {
+    dlog("Executing USER command\n");
+    // Check if the username is NULL
+    if (username == NULL) {
+        send_formatted(ss->fd, "-ERR Missing username\r\n");
+        return 1;  // Indicate failure
+    }
+    // Ensure that the command is only allowed in the Authorization state
+    if (ss->state != Authorization) {
+        send_formatted(ss->fd, "-ERR Command USER only allowed in AUTHORIZATION state\r\n");
+        return 1;  // No need to check, returning 1 to indicate failure anyway
+    }
+    // Validate the username using the is_valid_user function
+    if (is_valid_user(username, NULL)) {
+        // Store the username in the server state (for later use in PASS command)
+        strncpy(ss->current_user, username, sizeof(ss->current_user) - 1);
+        send_formatted(ss->fd, "+OK User accepted, proceed with PASS\r\n");
+        return 0;  // Indicate success
+    } else {
+        // Respond with an error if the user is not found
+        send_formatted(ss->fd, "-ERR user not found\r\n");
+        return 1;  // Indicate failure
+    }
 }
 
 int do_pass(serverstate *ss) {
@@ -111,13 +137,30 @@ int do_rset(serverstate *ss) {
     return 0;
 }
 
+// int do_noop(serverstate *ss) {
+//     dlog("Executing NOOP command\n");
+//     if (send_formatted(ss->fd, "+OK\r\n") <= 0) {
+//         return 1;  // Return 1 to indicate that the command was unsuccessful
+//     }
+//     return 0;  // Return 0 to indicate that the command was successful
+// }
+
 int do_noop(serverstate *ss) {
     dlog("Executing NOOP command\n");
-    if (send_formatted(ss->fd, "+OK\r\n") <= 0) {
-        return 1;  // Return 1 to indicate that the command was unsuccessful
+    // Ensure that the command is only allowed in the TRANSACTION state
+    if (ss->state != Transaction) {
+        // Send error response if not in TRANSACTION state
+        send_formatted(ss->fd, "-ERR Command NOOP only allowed in TRANSACTION state\r\n"); 
+        // No need to check return value since indicating failure anyway
+        return 1;  // Indicate failure
     }
-    return 0;  // Return 0 to indicate that the command was successful
+    // Send OK response if in the correct state
+    if (send_formatted(ss->fd, "+OK\r\n") <= 0) {
+        return 1;  // Indicate failure
+    }
+    return 0;  // Indicate success
 }
+
 
 int do_dele(serverstate *ss) {
     dlog("Executing dele\n");
@@ -166,13 +209,16 @@ void handle_client(void *new_fd) {
         int response = handle_command(ss, command);
         if (response == -1) {
             // Server should exit
+            dlog("Server should exit. received -1 from handle_command\n");
             break;
         } else if (response == 1) {
             // Command was unsuccessful
             // send_formatted(fd, "-ERR Command not recognized\r\n");
+            dlog("Received 1 from handle_command\n");
         } else {
             // Command was successful
             // send_formatted(fd, "+OK Command successful\r\n");
+            dlog("Received 0 from handle_command\n");
         }
     }
     // TODO: Clean up fields in `serverstate`, if required
@@ -183,12 +229,9 @@ void handle_client(void *new_fd) {
 
 int handle_command(serverstate *ss, const char *command) {
     // USER command can be handled in Authorization state
-    if (strcmp(command, "USER") == 0) {
-        if (checkstate(ss, Authorization)) {
-            // do_user(ss);
-        } else {
-            //
-        }
+    if (strcasecmp(command, "USER") == 0) {
+        // Pass NULL if no username is supplied
+        return do_user(ss, ss->words[1] ? ss->words[1] : NULL);
     }
     // QUIT command can be handled in any state
     else if (strcasecmp(command, "QUIT") == 0) {
@@ -226,15 +269,11 @@ int handle_command(serverstate *ss, const char *command) {
     }
     // NOOP command can be handled in Transaction state
     else if (strcasecmp(command, "NOOP") == 0) {
-        int state = checkstate(ss, Transaction);
-        if (state == 0) {
-            return do_noop(ss);
-        } else if (state == 1) {
-            dlog("Server is not in the appropriate state for the NOOP command\n");
-            return 1;
-        } else {
-            return -1;
-        }
+        return do_noop(ss);
+    } else {
+        // Command not recognized
+        send_formatted(ss->fd, "-ERR Command not recognized\r\n");
+        return 1;
     }
     return 1;
 }
